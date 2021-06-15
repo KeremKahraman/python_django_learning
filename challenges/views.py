9a343f8ce019b231c1b31fa07520f0f15619367a
keremkahraman <keremkahramanogullari@gmail.com>
2021-06-15 18:15:06 +0300

    Dynamic Path Segments, Captured values & Path Converters

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
@@ -9,3 +9,25 @@ def index(request):
 
 def february(request):
     return HttpResponse("This seems like also February works!")
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = "January"
+    elif month == 'february':
+        challenge_text = "February"
+    else:
+        return HttpResponseNotFound("No URL found") 
+    return HttpResponse(challenge_text)
+
+
+def monthly_challenge_by_number(request, month):
+    challenge_text = None
+    if month == 1:
+        challenge_text = "January"
+    elif month == 2:
+        challenge_text = "February"
+    else:
+        return HttpResponseNotFound("No URL found") 
+    return HttpResponse(challenge_text)
